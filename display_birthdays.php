6652a5840e813ac914475ec8d336b97b8fdbe0a1
EdenHamami <edenha1900@gmail.com>
2024-07-03 12:09:50 +0300

    display birth

---
+++ display_birthdays.php
@@ -0,0 +1,39 @@
+<?php
+require 'Database.php';
+
+$servername = "localhost";
+$username = "root";
+$password = "12345"; 
+$dbname = "test_db";
+
+$db = new Database($servername, $username, $password, $dbname);
+
+$current_month = date('m');
+
+$sql = "SELECT u.name, u.email, u.birthdate, p.title, p.body, p.created_at
+        FROM users u
+        JOIN posts p ON u.id = p.user_id
+        WHERE MONTH(u.birthdate) = $current_month AND u.active = 1 AND p.active = 1";
+
+$result = $db->select($sql);
+
+$image_path = 'images/default-avatar.jpg';
+
+if (!empty($result)) {
+    foreach ($result as $row) {
+        echo "<div class='user'>";
+        echo "<img src='$image_path' alt='User Avatar' style='width:50px;height:50px;'>";
+        echo "<h2>" . $row['name'] . "</h2>";
+        echo "<p>Email: " . $row['email'] . "</p>";
+        echo "<div class='post'>";
+        echo "<h3>" . $row['title'] . "</h3>";
+        echo "<p>" . $row['body'] . "</p>";
+        echo "<small>Posted on " . $row['created_at'] . "</small>";
+        echo "</div>";
+        echo "</div>";
+    }
+} else {
+    echo "No results found.";
+}
+
+?>
